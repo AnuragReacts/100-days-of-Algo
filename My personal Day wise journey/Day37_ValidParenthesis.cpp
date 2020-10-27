7b255c388b9a6032fe156af13bc46f6c972a4b34
hardik khanchandani <51117080+hardikhere@users.noreply.github.com>
2020-10-27 06:05:21 +0530

    Update Day37_ValidParenthesis.cpp

--- My personal Day wise journey/Day37_ValidParenthesis.cpp
+++ My personal Day wise journey/Day37_ValidParenthesis.cpp
@@ -1,70 +1,61 @@
-#include <bits/stdc++.h> 
-using namespace std; 
+class Solution
+{
+public:
+	bool isValid(string str)
+	{
+		int cnt = 0;
+		for (int i = 0; i < str.length(); i++)
+		{
+			if (str[i] == '(')
+				cnt++;
+			else if (str[i] == ')')
+				cnt--;
+			if (cnt < 0)
+				return false;
+		}
+		return (cnt == 0);
+	}
 
-bool isParenthesis(char c) 
-{ 
-	return ((c == '(') || (c == ')')); 
-} 
+	bool isParenthesis(char c)
+	{
+		return ((c == '(') || (c == ')'));
+	}
 
-
-bool isValidString(string str) 
-{ 
-	int cnt = 0; 
-	for (int i = 0; i < str.length(); i++) 
-	{ 
-		if (str[i] == '(') 
-			cnt++; 
-		else if (str[i] == ')') 
-			cnt--; 
-		if (cnt < 0) 
-			return false; 
-	} 
-	return (cnt == 0); 
-} 
-
-
-void removeInvalidParenthesis(string str) 
-{ 
-	if (str.empty()) 
-		return ; 
-
-	set<string> visit; 
-	queue<string> q; 
-	string temp; 
-	bool level; 
-
-	q.push(str); 
-	visit.insert(str); 
-	while (!q.empty()) 
-	{ 
-		str = q.front(); q.pop(); 
-		if (isValidString(str)) 
-		{ 
-			cout << str << endl; 
-			level = true; 
-		} 
-		if (level) 
-			continue; 
-		for (int i = 0; i < str.length(); i++) 
-		{ 
-			if (!isParenthesis(str[i])) 
-				continue; 
-			temp = str.substr(0, i) + str.substr(i + 1); 
-            cout<<"temp is "<<temp<<"\n";
-			if (visit.find(temp) == visit.end()) 
-			{ 
-				q.push(temp); 
-				visit.insert(temp); 
-			} 
-		} 
-	} 
-} 
-
-int main() 
-{ 
-	string expression = "()(()"; 
-	removeInvalidParenthesis(expression); 
-
-
-	return 0; 
-} 
+	vector<string> removeInvalidParentheses(string s)
+	{   
+		vector<string> ans;
+		queue<string> q;
+		set<string> visited;
+		q.push(s);
+		visited.insert(s);
+		bool level = false;
+		while (!q.empty())
+		{
+			string str = q.front();
+			q.pop();
+            //cout<<"str is "<<str<<endl;
+			if (isValid(str))
+			{
+				ans.push_back(str);
+				visited.insert(str);
+				level = true;
+			}
+			if (level)
+				continue;
+			for (int i = 0; i < str.length(); i++)
+			{
+				if (!isParenthesis(str[i]))
+					continue;
+				string temp = str.substr(0, i) + str.substr(i+1);
+               // cout<<"temp is "<<temp<<endl;
+				if (visited.find(temp) == visited.end())
+				{
+					//means if not visited
+					q.push(temp);
+					visited.insert(temp);
+				}
+			}
+		}
+		return ans;
+	}
+};
